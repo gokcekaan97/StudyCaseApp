f6d34426f2126e84a030bd981d1daeab24718d00
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-10-12 12:43:40 +0300

    initial Commit

---
+++ StudyCaseApp/StudyCaseApp/MainView/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaseStudy
+//
+//  Created by kaan gokcek on 11.10.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ StudyCaseApp/StudyCaseApp/MainView/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  CaseStudy
+//
+//  Created by kaan gokcek on 11.10.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+  
+}
